0c63ec979ded9ebe7785b28ca308849bc2474d5e
Peter_H <longqianh@gmail.com>
2023-03-14 09:54:22 +0800

    update README

--- test_slm.m
+++ test_slm.m
@@ -7,7 +7,7 @@ slm_para.fresh_time=1/60;
 slm_para.pixel_size=8e-6;
 
 slm=HoloeyeSLM(slm_para);      
-slm.blaze=slm.blazedgrating(1,0,40)/(2*pi)*255;
+slm.blaze=slm.blazedgrating(1,0,3);
 % slm.LUT=importdata('./data/lut.cfit');
 slm.disp_image(slm.init_image,1,1);
 
@@ -26,11 +26,6 @@ lut_path='C:\Program Files\Meadowlark Optics\Blink 1920 HDMI\LUT Files\1920x1152
 % lut_path=strcat(lib_dir,'slm4644_532.lut');
 slm=MeadowlarkSLM(slm_para,lib_dir,lut_path);
 slm.LUT=importdata('./data/lut.cfit');
-%%
-blaze=slm.blazedgrating(1,0,3)*1;% 220/255;
-slm.blaze=double(blaze);
-slm.disp_image(slm.init_image,1,1);
-% slm.disp_image(img,1,1);
 
 %% Meadowlark SLM (HDMI)
 % lib_dir = './utils/meadowlarkhdmi_sdk/';
@@ -43,12 +38,13 @@ slm_para.depth=8;
 slm_para.pixel_size=9.2e-6; 
 slm_para.bCppOrPython=false; 
 slm=MeadowlarkHDMISLM(slm_para,lib_dir,lut_path);
-% blaze_angle=100; % 1~255
-slm.blaze=slm.blazedgrating(-1,0,4)/(2*pi)*240/255; 
-% slm.disp_image(slm.init_image,0,1); % bug here
+
+%% blazedgrating setup
+
+blaze=slm.blazedgrating(1,0,3)*1; % 220/255;
+slm.blaze=double(blaze);
 slm.disp_image(slm.init_image,1,1);
- 
-% slm.clear_sdk();
+
 %% image display
 close all;
 img=imread('./data/vortex_6_19.bmp');
@@ -59,7 +55,7 @@ slm.disp_image(img,1,1);
 wavelength=532e-9;
 cam_pixel_size=8e-6;
 focal=300e-3;
-mag_prop=1; % ?
+mag_prop=1; %
 mag_img=0.5;
 
 close all;
@@ -67,8 +63,12 @@ star_img=imread('./data/star.png');
 star_img=mean(star_img,3);
 img_in=slm.GS_resample(star_img,wavelength,focal,cam_pixel_size,mag_img,mag_prop);
 star_phase=slm.GS(img_in,focal,'iter_num',20);
-% slm.disp_phase(star_phase,1,1);
+
 figure('Color','White');
 subplot(131);imshow(star_img,[]);
 subplot(132);imshow(img_in,[]);
 subplot(133);imshow(star_phase,[]);
+
+% slm.disp_phase(star_phase,1,1);
+%% unload
+slm.clear_sdk();
