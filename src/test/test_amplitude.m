bd4a63541a65d12ac35884acc5c31250b50a6deb
Caronkx <115875066+Caronkx@users.noreply.github.com>
2023-07-25 11:19:45 +0800

    add amplitude modulation

---
+++ src/test/test_amplitude.m
@@ -0,0 +1,18 @@
+clc;clear;close all;
+
+%% init Manager
+addpath("F:\Longqian\Projects\ExpManager\src"); % add ExpManager first
+
+exp_toolbox=["SLM-toolbox","Camera-toolbox"];
+ma=ExpManager('SLM-MMF',exp_toolbox);   
+ma.info()
+%%
+slm_para.height=1080;
+slm_para.width=1920;
+slm_para.fresh_time=1/60;
+slm_para.pixel_size=8e-6;
+
+slm=HoloeyeSLM(slm_para);      
+slm.blaze=slm.blazedgrating(1,0,6)*0.88;
+slm.LUT=importdata('../data/20230718-lut.cfit');
+slm.disp_image(slm.init_image,1);
