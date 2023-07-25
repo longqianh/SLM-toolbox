65ee41ed20df2af9d30c5d2682102acb3075aa24
Caronkx <115875066+Caronkx@users.noreply.github.com>
2023-07-25 11:33:54 +0800

    update test_amplitude

--- src/test/test_amplitude.m
+++ src/test/test_amplitude.m
@@ -2,7 +2,7 @@ clc;clear;close all;
 
 %% init Manager
 addpath("F:\Longqian\Projects\ExpManager\src"); % add ExpManager first
-
+% addpath("F:\KangYuan\project\ExpManager\src"); 
 exp_toolbox=["SLM-toolbox","Camera-toolbox"];
 ma=ExpManager('SLM-MMF',exp_toolbox);   
 ma.info()
@@ -16,3 +16,7 @@ slm=HoloeyeSLM(slm_para);
 slm.blaze=slm.blazedgrating(1,0,6)*0.88;
 slm.LUT=importdata('../data/20230718-lut.cfit');
 slm.disp_image(slm.init_image,1);
+%% 
+phase = zeros([512,512]);
+amp = imread("lena_gray_512.tif");imshow(amp,[]);
+slm.disp_phase(phase,1,"amp",amp);
